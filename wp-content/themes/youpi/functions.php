c421e4f1c6cd0162bc0d3e77de7a39ed96d27025
Boubacar <diallobouba905@gmail.com>
2020-10-29 16:18:39 +0100

    Mise à jour 2

--- wp-content/themes/youpi/functions.php
+++ wp-content/themes/youpi/functions.php
@@ -1,9 +1,11 @@
 <?php
+//***mettre en gras les ecritures ****/
 function excerpt_display_strong($texte) 
  {
      return "<strong>".$texte."</strong>";
  }
  add_filter('get_the_excerpt', 'excerpt_display_strong');
+ //***la fonction qui créer le logo du site ****/
 
  function youpi_custom_logo_setup() 
 {
@@ -21,9 +23,12 @@ function excerpt_display_strong($texte)
 
 add_action( 'after_setup_theme', 'youpi_custom_logo_setup' );
 
+//***la création du menu *****/
+
 add_theme_support('menus');  
 
 register_nav_menu("principal", "Menu principal");
+//***la fonction pour la navigation ****/
 
 function register_navwalker() 
 {
